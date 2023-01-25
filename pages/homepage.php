084bd8d66d3c6806c0ccbd309779e4f5dca138cc
mattia-gallinaro <gallinaromattia6@gmail.com>
2023-01-25 09:25:10 +0100

    cominciato login e homepage web app utente

---
+++ pages/homepage.php
@@ -0,0 +1,13 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Lo scam</title>
+    </head>
+    <body>
+        <h1>SIUUUUUUUUUUUUUUUUUU</h1>
+    </body>
+</html>
---
+++ php/login.php
@@ -0,0 +1,6 @@
+<?php
+function login($username, $password){
+    //$url = 'https://localhost/food-api/API/login/';
+    header('Location: http://localhost/Web_App_User/pages/homepage.php');
+}
+?>
