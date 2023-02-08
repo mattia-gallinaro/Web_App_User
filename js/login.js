06ba623a734d6945414271d924963a9053d26e41
mattia-gallinaro <gallinaromattia6@gmail.com>
2023-02-08 21:21:21 +0100

    mini continuo del login

--- js/login.js
+++ js/login.js
@@ -1,9 +1,22 @@
 let form_log = null;
+let email_box = null;
+let paswd_box = null;
 $(document).ready(function() {
     form_log = document.getElementById('name');
+    email_box = document.getElementById('email');
+    paswd_box = document.getElementById('password');
 });
 
 
-function login(){
-    
+async function login(){
+    const json =  "{ id : " + email_box.textContent + ", password : " + paswd_box.textContent +"}";
+    const response = await fetch("http://localhost/food-api/API/user/login.php", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body  : JSON.stringify(json)
+    }).then((response) =>{
+        response.JSON();
+    }).then((data)=> console.log(data));
 }
