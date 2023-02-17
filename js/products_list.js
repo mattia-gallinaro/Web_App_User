f218ba788bc2a8d71a26e74a0389ce3ba5068a2c
mattia-gallinaro <gallinaromattia6@gmail.com>
2023-02-17 21:41:15 +0100

    terminare ordinazione prodotti

--- js/products_list.js
+++ js/products_list.js
@@ -40,17 +40,64 @@ function showProducts(products){
             if(value_session == -1){
                 const btn_plus = document.createElement('button');
                 btn_plus.innerHTML = '+';
+                btn_plus.addEventListener('click', function(){
+                    var value = parseInt(qnt.innerHTML);
+                    if(value == 68){
+                        window.location.replace('http://localhost/Web_App_User/pages/login.php');
+                    }
+                    value++;
+                    console.log(value);
+                    qnt.innerHTML = value.toString();
+                    
+                });
                 element.appendChild(btn_plus);
                 const qnt = document.createElement('div');
-                qnt.innerHTML = 0;
+                qnt.innerHTML += 0;
                 element.appendChild(qnt);
                 const btn_less = document.createElement('button');
+                btn_less.addEventListener('click', function(){
+                    var value = parseInt(qnt.innerHTML);
+                    if(value == 68){
+                        window.location.replace('http://localhost/Web_App_User/pages/login.php');
+                    }
+                    if(value > 0 ){
+                        value--;
+                    }
+                    console.log(value);
+                    qnt.innerHTML = value.toString();
+                })
                 btn_less.innerHTML = '-';
                 element.appendChild(btn_less);
+                const order =  document.createElement('button');
+                order.addEventListener('click', function(){
+                    //console.log(qnt.innerHTML);
+                    //console.log(element.getAttribute('value'));
+                    //addProductCart(parseInt(qnt.textContent), element.getAttribute('value'));
+                });
+                order.innerHTML = 'ORDINA ORDINAA';
+                element.appendChild(order);
             }
             //var this_el = document.getElementsByClassName('product');
             //var image = products[i].
             //this_el[length(this_el) - 1].appendChild();
         }
     }
+}
+
+function addProductCart(id, name){
+    var response = fetch('https://localhost/food-api/API/cart/addCartItem.php', {
+        method: 'POST', 
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify((id, name)),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log('Success:', data);
+        })
+        .catch((error) => {
+          console.error('Error:', error);
+        });
+      
 }
